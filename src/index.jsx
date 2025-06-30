8890bcc164b6ba8f94331cb522e986d79e0e5ee1
Daniil Fedotov <101014949+VeganCrypt@users.noreply.github.com>
2025-06-30 19:49:32 +0300

    update container

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,10 @@ import Home from "./components/screens/Home/Home";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="py-10 bg-gray-900 h-screen">
+    <div className="py-10 bg-gray-900 min-h-screen">
+      <div className="container mx-auto px-4">
       <Home />
+      </div>
     </div>
   </React.StrictMode>
 );
