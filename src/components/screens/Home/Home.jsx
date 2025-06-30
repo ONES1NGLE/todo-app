7636200cde2b6ff307877522f375c772e64ab780
Daniil Fedotov <101014949+VeganCrypt@users.noreply.github.com>
2025-06-30 19:22:26 +0300

    Update todo app

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -51,7 +51,7 @@ const Home = () => {
   return (
     <div className="text-white w-4/5 mx-auto">
       <h1 className="text-2xl font-bold text-center mb-10">
-        Todo for JUNIOR++
+        TODO LIST
       </h1>
       {todos.map((todo) => (
         <TodoItem
